5772e40caa6bbb2db153ab4f72d7ebdcb93cb7e3
brajesh <brajesh.singhania143@gmail.com>
2026-08-13 15:30:09 +0530

    Add document loaders for CSV, PDF, text, and web sources; include sample usage

---
+++ langchain-basics/RagComponents/docloader/text_loader.py
@@ -0,0 +1,15 @@
+from langchain_community.document_loaders import TextLoader
+
+loader = TextLoader("cricket.txt", encoding='utf-8')
+
+docs = loader.load()
+
+docs = loader.load()
+
+print(type(docs))
+
+print(len(docs))
+
+print(docs[0].page_content)
+
+print(docs[0].metadata)
